138c8bc000eeb93a6327528909ac0589813eb565
Ayush Kumar <eliasakeleven@gmail.com>
2020-12-06 05:00:33 +0530

    Getting data from Parse Server

--- app/src/main/java/com/ak11/instagramclone/SignUp.java
+++ app/src/main/java/com/ak11/instagramclone/SignUp.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUp extends AppCompatActivity {
 
     private Button btn;
     private EditText edtName, edtPower, edtSpeed, edtType;
+    private TextView txtGetData;
+    private String strcar;
 
 
     @Override
@@ -28,6 +36,40 @@ public class SignUp extends AppCompatActivity {
         edtType=findViewById(R.id.edtType);
         edtPower=findViewById(R.id.edtPower);
         edtSpeed=findViewById(R.id.edtSpeed);
+
+        strcar="";
+
+        txtGetData = findViewById(R.id.textGetData);
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignUp.this,"Getting data...",Toast.LENGTH_SHORT).show();
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Car");
+                parseQuery.findInBackground(new FindCallback<ParseObject>(){
+
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+
+                        if(e==null)
+                            if(objects.size()>0){
+
+                                for(ParseObject obj:objects){
+                                    strcar = strcar+obj.get("car_name")+"\n";
+                                }
+                                txtGetData.setText(strcar);
+                                FancyToast.makeText(SignUp.this,"Data retrieved successfully",
+                                        FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true)
+                                        .show();
+                            }
+                        else
+                                FancyToast.makeText(SignUp.this,e.getMessage()+"\nError Code"+e.getCode(),
+                                        FancyToast.LENGTH_LONG,FancyToast.ERROR,true)
+                                        .show();
+
+                    }
+                });
+            }
+        });
     }
     public void btnIsTapped(View v){
         ParseObject car = new ParseObject("Car");

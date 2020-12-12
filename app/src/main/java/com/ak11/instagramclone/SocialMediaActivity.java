6927f11f21b6a93f57caf8fd0076866f88a1dfb0
Ayush Kumar <eliasakeleven@gmail.com>
2020-12-13 04:28:56 +0530

    Layout Created till Page Adapter

--- app/src/main/java/com/ak11/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/ak11/instagramclone/SocialMediaActivity.java
@@ -1,15 +1,33 @@
 package com.ak11.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class SocialMediaActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class SocialMediaActivity extends AppCompatActivity {
+     private Toolbar toolbar;
+     private ViewPager viewPager;
+     private TabLayout tabLayout;
+     private  TabAdapter tabAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        setTitle("Social Media App!");
+
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,false);
 
     }
 }
--- app/src/main/java/com/ak11/instagramclone/TabAdapter.java
+++ app/src/main/java/com/ak11/instagramclone/TabAdapter.java
@@ -1,6 +1,7 @@
 package com.ak11.instagramclone;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -13,12 +14,38 @@ public class TabAdapter extends FragmentPagerAdapter {
 
     @NonNull
     @Override
-    public Fragment getItem(int position) {
-        return null;
+    public Fragment getItem(int tabPosition) {
+        switch(tabPosition){
+            case 0:
+                ProfileTab profileTab = new ProfileTab();
+                return profileTab;
+            case 1:;
+                return  new UsersTab();
+            case 2:
+                return new SharePictureTab();
+            default:
+                return null;
+        }
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch ( position) {
+            case 0:
+                return "Profile";
+            case 1:
+                return "Users";
+            case 2:
+                return "Share Picture";
+            default:
+                return null;
+        }
+
     }
 }

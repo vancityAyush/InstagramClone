9fff48ebbf5c5d685f0415dd6ffebe26435168f7
Ayush Kumar <eliasakeleven@gmail.com>
2020-12-14 03:16:01 +0530

    Shared Pictueres Tab Half Implemented

--- app/src/main/java/com/ak11/instagramclone/SharePictureTab.java
+++ app/src/main/java/com/ak11/instagramclone/SharePictureTab.java
@@ -1,64 +1,79 @@
 package com.ak11.instagramclone;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SharePictureTab#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SharePictureTab extends Fragment {
+public class SharePictureTab extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageView imgShare;
+    private EditText edtDescription;
+    private Button btnShareImage;
 
     public SharePictureTab() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SharePictureTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SharePictureTab newInstance(String param1, String param2) {
-        SharePictureTab fragment = new SharePictureTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share_picture_tab, container, false);
+        View view =  inflater.inflate(R.layout.fragment_share_picture_tab, container, false);
+
+
+        imgShare = view.findViewById(R.id.imgShare);
+        edtDescription = view.findViewById(R.id.edtDescription);
+        btnShareImage = view.findViewById(R.id.btnShareImage);
+
+        imgShare.setOnClickListener(SharePictureTab.this);
+        btnShareImage.setOnClickListener(SharePictureTab.this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgShare:
+                if(Build.VERSION.SDK_INT>=23 && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                     requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1000);
+                }
+                else{
+                    getChosenImage();
+                }
+                break;
+            case R.id.btnShareImage:
+
+                break;
+        }
+
+    }
+
+    private void getChosenImage() {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1000){
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                getChosenImage();
+            }
+        }
     }
 }
